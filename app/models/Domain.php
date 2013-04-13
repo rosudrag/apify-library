3db6b5c2e1cda647e9dd5a6b51562c495c721a8a
dragos <r.dragosh@yahoo.com>
2013-04-13 20:00:37 +0100

    debug 4

--- app/models/Domain.php
+++ app/models/Domain.php
@@ -4,4 +4,15 @@ class Domain extends Entity
     protected $domainID;
     protected $name;
     
+    // sanitize and validate name (optional) 
+    public function setName($value)
+    {
+        $value = htmlspecialchars(trim($value), ENT_QUOTES);
+        if (empty($value) || strlen($value) < 3) {
+            throw new ValidationException('Invalid name');
+        }
+        $this->name = $value;
+    }
+    
+
 }
